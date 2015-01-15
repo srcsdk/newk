23889eb32d4066a3760668cf6038c9328a573218
srcsdk <srcsdk@users.noreply.github.com>
2015-01-15 16:40:00 -0500

    handle errors and timeouts

--- read.sh
+++ read.sh
@@ -1,8 +1,19 @@
 #!/bin/bash
-while read url; do
+if [ ! -f feeds.txt ]; then
+    echo "feeds.txt not found"
+    exit 1
+fi
+
+while read -r url; do
+    [ -z "$url" ] && continue
     echo ""
-    echo "=== $url ==="
-    echo ""
-    curl -s "$url" | grep -oP '(?<=<title>).*?(?=</title>)' | head -10
-    echo ""
+    echo "[$url]"
+    result=$(curl -s --max-time 10 "$url" 2>/dev/null | grep -oP '(?<=<title>).*?(?=</title>)' | head -10)
+    if [ -z "$result" ]; then
+        echo "  (no results)"
+    else
+        echo "$result" | while read -r line; do
+            echo "  $line"
+        done
+    fi
 done < feeds.txt
