1544d431c73e4b70588fdc8a7b85f3f710af1940
srcsdk <srcsdk@users.noreply.github.com>
2020-10-30 22:05:00 -0500

    basic express server with feed listing endpoint

---
+++ gui/server.js
@@ -0,0 +1,63 @@
+const express = require('express');
+const fs = require('fs');
+const path = require('path');
+const { execSync } = require('child_process');
+
+const app = express();
+const port = process.argv.includes('--port')
+    ? parseInt(process.argv[process.argv.indexOf('--port') + 1])
+    : 3000;
+
+const feeds_file = path.join(__dirname, '..', 'feeds.txt');
+const research_file = path.join(__dirname, '..', 'research_feeds.txt');
+
+function load_feeds(filepath) {
+    if (!fs.existsSync(filepath)) return [];
+    return fs.readFileSync(filepath, 'utf8')
+        .split('\n')
+        .map(l => l.trim())
+        .filter(l => l && !l.startsWith('#'));
+}
+
+app.get('/api/feeds', (req, res) => {
+    const feeds = load_feeds(feeds_file);
+    const research = load_feeds(research_file);
+    res.json({
+        feeds: feeds,
+        research: research,
+        total: feeds.length + research.length,
+    });
+});
+
+app.get('/api/scrape', (req, res) => {
+    try {
+        const scraper = path.join(__dirname, '..', 'scrape.py');
+        const output = execSync(
+            `python3 ${scraper} --json --limit 20`,
+            { timeout: 60000, encoding: 'utf8' }
+        );
+        res.json(JSON.parse(output));
+    } catch (err) {
+        res.status(500).json({ error: 'scrape failed', detail: err.message });
+    }
+});
+
+app.get('/', (req, res) => {
+    res.send(`
+        <html>
+        <head><title>newk</title></head>
+        <body style="background:#1e1e1e;color:#ccc;font-family:monospace;padding:20px">
+            <h1>newk feed reader</h1>
+            <p>endpoints:</p>
+            <ul>
+                <li><a href="/api/feeds" style="color:#4ec9b0">/api/feeds</a> - list all feeds</li>
+                <li><a href="/api/scrape" style="color:#4ec9b0">/api/scrape</a> - scrape recent items</li>
+            </ul>
+        </body>
+        </html>
+    `);
+});
+
+app.listen(port, () => {
+    console.log(`newk server running on http://localhost:${port}`);
+});
