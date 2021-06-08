5bfffc8b0a6879eaa0a161849c128c400b04dc8e
srcsdk <srcsdk@users.noreply.github.com>
2021-06-07 21:25:00 -0500

    add duplicate headline detection across sources

--- dedup.py
+++ dedup.py
@@ -81,6 +81,32 @@ def deduplicate(articles, threshold=0.7):
     return unique
 
 
+def detect_similar_headlines(headline, existing, threshold=0.6):
+    """find headlines similar to the given one using jaccard similarity.
+
+    compares word sets between the headline and each existing headline.
+    returns a list of (existing_headline, score) tuples that meet
+    the threshold.
+    """
+    headline_words = set(normalize_text(headline).split())
+    if not headline_words:
+        return []
+
+    matches = []
+    for other in existing:
+        other_words = set(normalize_text(other).split())
+        if not other_words:
+            continue
+        intersection = headline_words & other_words
+        union = headline_words | other_words
+        score = len(intersection) / len(union)
+        if score >= threshold:
+            matches.append((other, round(score, 3)))
+
+    matches.sort(key=lambda x: x[1], reverse=True)
+    return matches
+
+
 def merge_feeds(feed_lists, threshold=0.7):
     """merge multiple feed lists and deduplicate"""
     all_articles = []
