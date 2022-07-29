98e6f7acff650d6a7e440d55849b6dbafd6543ea
srcsdk <srcsdk@users.noreply.github.com>
2022-07-28 22:15:00 -0600

    add reading statistics and analytics tracking

--- stats.py
+++ stats.py
@@ -1,120 +1,94 @@
 #!/usr/bin/env python3
-"""feed statistics dashboard - articles per day, top sources"""
+"""reading statistics and analytics"""
 
 import json
-import time
-from pathlib import Path
-
-STATS_DIR = Path.home() / ".config" / "newk"
-STATS_FILE = STATS_DIR / "feed_stats.json"
+import os
+from collections import Counter
 
 
-def load_stats():
-    """load accumulated statistics"""
-    if not STATS_FILE.exists():
-        return {"daily": {}, "sources": {}, "categories": {}, "total_articles": 0}
-    try:
-        with open(STATS_FILE) as f:
-            return json.load(f)
-    except (json.JSONDecodeError, OSError):
-        return {"daily": {}, "sources": {}, "categories": {}, "total_articles": 0}
+class ReadingStats:
+    """track and analyze reading statistics."""
 
+    def __init__(self, data_dir=None):
+        if data_dir is None:
+            data_dir = os.path.expanduser("~/.newk")
+        self.stats_file = os.path.join(data_dir, "stats.json")
+        os.makedirs(data_dir, exist_ok=True)
+        self.data = self._load()
 
-def save_stats(stats):
-    """save statistics to disk"""
-    STATS_DIR.mkdir(parents=True, exist_ok=True)
-    with open(STATS_FILE, "w") as f:
-        json.dump(stats, f, indent=2)
+    def _load(self):
+        if os.path.isfile(self.stats_file):
+            with open(self.stats_file) as f:
+                return json.load(f)
+        return {"daily": {}, "sources": {}, "categories": {}, "total_read": 0}
 
+    def _save(self):
+        with open(self.stats_file, "w") as f:
+            json.dump(self.data, f, indent=2)
 
-def record_articles(articles):
-    """record article statistics from a fetch"""
-    stats = load_stats()
-    today = time.strftime("%Y-%m-%d")
-
-    if today not in stats["daily"]:
-        stats["daily"][today] = 0
-    stats["daily"][today] += len(articles)
-    stats["total_articles"] += len(articles)
-
-    for article in articles:
+    def record(self, article, date=None):
+        """record reading an article."""
+        import time
+        if date is None:
+            date = time.strftime("%Y-%m-%d")
+        self.data["daily"][date] = self.data["daily"].get(date, 0) + 1
         source = article.get("source", "unknown")
-        if source not in stats["sources"]:
-            stats["sources"][source] = 0
-        stats["sources"][source] += 1
+        self.data["sources"][source] = self.data["sources"].get(source, 0) + 1
+        category = article.get("category", "general")
+        self.data["categories"][category] = self.data["categories"].get(category, 0) + 1
+        self.data["total_read"] = self.data.get("total_read", 0) + 1
+        self._save()
 
-        category = article.get("category", "uncategorized")
-        if category not in stats["categories"]:
-            stats["categories"][category] = 0
-        stats["categories"][category] += 1
+    def daily_average(self):
+        """average articles read per day."""
+        daily = self.data.get("daily", {})
+        if not daily:
+            return 0
+        return round(sum(daily.values()) / len(daily), 1)
 
-    # trim daily stats to last 90 days
-    sorted_days = sorted(stats["daily"].keys())
-    if len(sorted_days) > 90:
-        for day in sorted_days[:-90]:
-            del stats["daily"][day]
+    def top_sources(self, n=5):
+        """most read sources."""
+        sources = self.data.get("sources", {})
+        return sorted(sources.items(), key=lambda x: x[1], reverse=True)[:n]
 
-    save_stats(stats)
-    return stats
+    def top_categories(self, n=5):
+        """most read categories."""
+        cats = self.data.get("categories", {})
+        return sorted(cats.items(), key=lambda x: x[1], reverse=True)[:n]
 
+    def streak(self):
+        """current reading streak in days."""
+        import time
+        daily = self.data.get("daily", {})
+        if not daily:
+            return 0
+        today = time.strftime("%Y-%m-%d")
+        count = 0
+        current = today
+        while current in daily:
+            count += 1
+            parts = current.split("-")
+            from datetime import date, timedelta
+            d = date(int(parts[0]), int(parts[1]), int(parts[2]))
+            d -= timedelta(days=1)
+            current = d.strftime("%Y-%m-%d")
+        return count
 
-def top_sources(n=10):
-    """get top n sources by article count"""
-    stats = load_stats()
-    sorted_sources = sorted(stats["sources"].items(),
-                            key=lambda x: x[1], reverse=True)
-    return sorted_sources[:n]
-
-
-def top_categories(n=10):
-    """get top n categories by article count"""
-    stats = load_stats()
-    sorted_cats = sorted(stats["categories"].items(),
-                         key=lambda x: x[1], reverse=True)
-    return sorted_cats[:n]
-
-
-def daily_average(days=30):
-    """calculate average articles per day over recent period"""
-    stats = load_stats()
-    daily = stats.get("daily", {})
-    sorted_days = sorted(daily.keys())[-days:]
-    if not sorted_days:
-        return 0
-    total = sum(daily[d] for d in sorted_days)
-    return total / len(sorted_days)
-
-
-def weekly_trend():
-    """show articles per day for the last 7 days"""
-    stats = load_stats()
-    daily = stats.get("daily", {})
-    sorted_days = sorted(daily.keys())[-7:]
-    return [(d, daily.get(d, 0)) for d in sorted_days]
+    def summary(self):
+        """generate reading summary."""
+        return {
+            "total_articles": self.data.get("total_read", 0),
+            "daily_average": self.daily_average(),
+            "days_tracked": len(self.data.get("daily", {})),
+            "top_sources": self.top_sources(3),
+            "top_categories": self.top_categories(3),
+            "current_streak": self.streak(),
+        }
 
 
-def format_dashboard():
-    """format statistics for display"""
-    stats = load_stats()
-    lines = ["feed statistics dashboard", ""]
-
-    lines.append(f"total articles tracked: {stats['total_articles']}")
-    lines.append(f"average per day (30d): {daily_average():.1f}")
-    lines.append("")
-
-    lines.append("top sources:")
-    for source, count in top_sources(5):
-        lines.append(f"  {source}: {count}")
-    lines.append("")
-
-    lines.append("top categories:")
-    for cat, count in top_categories(5):
-        lines.append(f"  {cat}: {count}")
-    lines.append("")
-
-    lines.append("recent daily:")
-    for day, count in weekly_trend():
-        bar = "#" * min(count, 50)
-        lines.append(f"  {day}: {count:>4} {bar}")
-
-    return "\n".join(lines)
+if __name__ == "__main__":
+    rs = ReadingStats("/tmp/newk_stats")
+    rs.record({"source": "hackernews", "category": "tech"})
+    rs.record({"source": "arxiv", "category": "science"})
+    rs.record({"source": "hackernews", "category": "tech"})
+    print(f"summary: {rs.summary()}")
